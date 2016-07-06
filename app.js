2427eb3d0a31304f232fc986cbe8881866e2c6a8
Caryn Ligon <carynligon@gmail.com>
2016-07-06 14:55:26 -0500

    can add new list item, delete, and object updates completes

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var $inputEl = $('#user-input');
 var $userInput;
 var data = [];
-
-function makeLI(item, i) {
-  var $newLI = $('<li class="list-item"></li>');
-  $newLI.text(item.task);
-  $('#task-list').append($newLI);
-  $newLI.append('<input type="checkbox" id="check" />');
-  $newLI.append('<i class="fa fa-trash-o" id="trash" aria-hidden="true"></i>');
-}
+var $taskToDelete;
+var $trash = ('.trash');
 
 var getReq = {
 url: 'http://tiny-za-server.herokuapp.com/collections/caryns-to-dos',
@@ -18,11 +12,60 @@ success: function (response) {
   data = response;
   $('#task-list').empty();
   data.forEach(makeLI);
+  $('.trash').on('click', function () {
+    trashHandler($(this));
+  });
+  $('.check').change(function (){
+    if ($(this).is(':checked')) {
+      checkboxHandler($(this));
+    }
+  });
   }
 };
 
-$.ajax(getReq);
+function trashHandler (item) {
+  var $deleteID = data[item.parent('li').index()]._id;
 
+  var deleteTask = {
+    url: 'http://tiny-za-server.herokuapp.com/collections/caryns-to-dos/' + $deleteID,
+    type: 'DELETE',
+    dataType: 'json',
+    success: function (response) {
+      console.log(response);
+      }
+  };
+
+  $.ajax(deleteTask);
+  $.ajax(getReq);
+}
+
+function checkboxHandler (item) {
+  var $checkedID = data[item.parent('li').index()]._id;
+  console.log($checkedID);
+
+  var checkTask = {
+    url: 'http://tiny-za-server.herokuapp.com/collections/caryns-to-dos/' + $checkedID,
+    type: 'PUT',
+    dataType: 'json',
+    success: function(response) {
+      console.log(response);
+    },
+    data: {
+      completed: true
+    }
+  };
+  $.ajax(checkTask);
+}
+
+function makeLI(item, i) {
+  var $newLI = $('<li class="list-item"></li>');
+  $newLI.text(item.task);
+  $('#task-list').append($newLI);
+  $newLI.append('<input type="checkbox" class="check" />');
+  $newLI.append('<i class="fa fa-trash-o trash" aria-hidden="true"></i>');
+}
+
+$.ajax(getReq);
 
 
 $inputEl.on('keyup', function (evt) {
@@ -42,8 +85,4 @@ $inputEl.on('keyup', function (evt) {
     });
     $.ajax(getReq);
   }
-});
-
-$('#trash').on('click', function() {
-  
 });
